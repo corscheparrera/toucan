1a5b67956fc2395583aedbb5b39680c607bc1299
Maxime Lussier <m.lussier@live.ca>
2023-06-17 19:15:41 -0400

    toucan !!

--- packages/client/src/components/organisms/Header/index.tsx
+++ packages/client/src/components/organisms/Header/index.tsx
@@ -1,9 +1,9 @@
 import classNames from "classnames";
 import React from "react";
+import img from "../../../assets/img/toucan.webp";
 import Date, { Size as DateSize } from "../../atoms/Date";
 import P, { Size } from "../../atoms/P";
 import styles from "./style.module.css";
-
 interface IProps {
   className?: string;
 }
@@ -13,6 +13,7 @@ const Header: React.FC<IProps> = ({ className }) => {
 
   return (
     <div className={classProps}>
+      <img className={styles["toucan-img"]} src={img} />
       <P text="Toucan" size={Size.extraLarge} className={styles["title"]} />
       <Date className={styles["date"]} size={DateSize.small} />
     </div>
